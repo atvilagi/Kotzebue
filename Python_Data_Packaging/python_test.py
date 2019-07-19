fff7649b023f8f9aa5395dea9d7a868526c491e6
Doug Keller <dg.kllr.jr@gmail.com>
2019-07-19 15:41:39 -0400

    Update python_test.py

--- Python_Data_Packaging/python_test.py
+++ Python_Data_Packaging/python_test.py
@@ -8,3 +8,5 @@ file_path = os.path.abspath(os.path.dirname(__file__))
 new_nc = os.path.join(file_path,'..','Data','netCDF_Files','puma_unified_data.nc')
 merged_file = Dataset(new_nc,'r',format='NETCDF4')
 
+print(new_nc['ATV000'].getncattrs('Location'))
+print(new_nc['ATV000'])
