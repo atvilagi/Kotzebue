7f9fc9fe80acd87971febd4fef7edbfa99a6020f
Doug Keller <dg.kllr.jr@gmail.com>
2019-07-19 15:39:40 -0400

    Create python_test.py

---
+++ Python_Data_Packaging/python_test.py
@@ -0,0 +1,10 @@
+#By: Doug
+#Created: Fri Jul 19 15:38:17 2019
+
+from netCDF4 import Dataset
+
+file_path = os.path.abspath(os.path.dirname(__file__))
+
+new_nc = os.path.join(file_path,'..','Data','netCDF_Files','puma_unified_data.nc')
+merged_file = Dataset(new_nc,'r',format='NETCDF4')
+
