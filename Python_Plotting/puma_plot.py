1b561a8990242ea564812027a6d4ee3aa3a9b3df
dkllrjr <dg.kllr.jr@gmail.com>
2019-07-29 10:09:19 -0800

    updating

--- Python_Plotting/puma_plot.py
+++ Python_Plotting/puma_plot.py
@@ -3,6 +3,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def time2theta_time(t,t_ind):
+    # expecting seconds
+    day = 86400 #seconds in a day
+    flag = np.where(t<t[t_ind]+day)[0]
+        
+#    t_theta = t[t_ind:t_ind+i]*2*np.pi/day
+    
+#    print(i)
+#    return t_theta,i    
+    return flag
+    
 def polar_temp():
-    
-    
+    pass
--- Python_Plotting/python_test.py
+++ Python_Plotting/python_test.py
@@ -7,6 +7,7 @@ import os
 import numpy as np
 from datetime import datetime
 import pytz
+import puma_plot as pplot
 
 file_path = os.path.abspath(os.path.dirname(__file__))
 
@@ -29,17 +30,29 @@ for stove in stoves:
     
     print(time,inT)
     
-theta = np.linspace(0,2*np.pi,1000)
-r = np.sin(theta)
+flag = np.where(time<time[100]+86400)[0]
 
-theta_loc = np.linspace(0,2*np.pi,25)*180/np.pi
-theta_loc = theta_loc[0:-1]
-theta_lab = []
-for i in range(24):
-    theta_lab.append(str(i) + ':00')
+print(flag)
+#t_theta = pplot.time2theta_time(np.array(time),0)
 
-# Figure out how to determine a new day in the datetime object for making the polar plots
-# Then use the .seconds feature per 86400 seconds to determine a full day (or use datetime)
-plt.polar(theta,r)
-plt.thetagrids(theta_loc,labels = theta_lab)
+time2 = time[0:100]
+inT2 = inT[0:100]
+plt.polar(time2,inT2)
 plt.show()
+
+#### Test Plotting ####
+    
+#theta = np.linspace(0,2*np.pi,1000)
+#r = np.sin(theta)
+#
+#theta_loc = np.linspace(0,2*np.pi,25)*180/np.pi
+#theta_loc = theta_loc[0:-1]
+#theta_lab = []
+#for i in range(24):
+#    theta_lab.append(str(i) + ':00')
+#
+## Figure out how to determine a new day in the datetime object for making the polar plots
+## Then use the .seconds feature per 86400 seconds to determine a full day (or use datetime)
+#plt.polar(theta,r)
+#plt.thetagrids(theta_loc,labels = theta_lab)
+#plt.show()
