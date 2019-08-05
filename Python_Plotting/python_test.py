20fe2adc63fc26ee5d8d20536b1eae2dd506bf18
dkllrjr <dg.kllr.jr@gmail.com>
2019-08-05 09:52:58 -0800

    added ploting, now for the jupyter notebooks

--- Python_Plotting/python_test.py
+++ Python_Plotting/python_test.py
@@ -28,71 +28,28 @@ stoves = list(uni_nc.groups)
 #    clicks = uni_nc[stove]['clicks'][:]
 #    cumclicks = uni_nc[stove]['cumulative_clicks'][:]
     
-stove = stoves[2]
+stove = stoves[1]
 print(stove)
 time = uni_nc[stove]['time'][:]
 t_datetime = pplot.timestamp2datetime(time)
 inT = uni_nc[stove]['indoor_temp'][:]
+outT = uni_nc[stove]['outdoor_temp'][:]
 gph = uni_nc[stove]['fuel_consumption_rate'][:]
 clicks = uni_nc[stove]['clicks'][:]
 cumclicks = uni_nc[stove]['cumulative_clicks'][:]
 
-for i in range(len(gph)):
-    if gph[i] > 10:
-        print('yikes')
-        gph[i] = 0
-
 months = pplot.months_available(t_datetime)
 print(months)
 
-#for i in range(len(clicks)):
-#    print(time[i],cumclicks[i],clicks[i],gph[i])
+gph = np.array(gph)
+gph,_,_ = pplot.brock_improved_despike(gph,1)
 
-pplot.polar_flow_plot_per_month(stove,2019,2,t_datetime,gph,'test.png')
-pplot.polar_flow_plot_average_per_month(stove,2019,2,t_datetime,gph,30,'test2.png')
+#### Plotting Heat Degree Days ####
 
-#for i in range(len(clicks)):
-#    print(clicks[i],cumclicks[i],gph[i])
+hdd = pplot.heat_degree_day(t_datetime,outT)
+pplot.plot_heating_degree_days(stove,t_datetime,gph,hdd,'test_hdd.png')
 
-#### By day plot ####
+#### Plotting Polar #####
 
-#i,j = pplot.time_day_segment(time)
-#
-#t = time[i[0]:j[0]]
-#inT_s = inT[i[0]:j[0]]
-#gph_s = gph[i[0]:j[0]]
-#
-#t_s = pplot.time2timestamp(t)
-#t_theta = pplot.time2theta_time(t)
-#
-#a = datetime(2019,2,15,0,0,0)
-#a = timeAK.localize(a)
-#b = t_stamp[0]
-#c = a - b
-#
-#days,day_ind = pplot.time2days(t_s,a)
-#
-#t_day = t[day_ind[1]:day_ind[2]]
-#t_theta = pplot.time2theta_time(t_day)
-#inT_theta = inT_s[day_ind[1]:day_ind[2]]
-#gph_theta = gph_s[day_ind[1]:day_ind[2]]
-#
-#plt.polar(t_theta,gph_theta)
-#plt.show()
-
-#### Test Plotting ####
-    
-#theta = np.linspace(0,2*np.pi,1000)
-#r = np.sin(theta)
-#
-#theta_loc = np.linspace(0,2*np.pi,25)*180/np.pi
-#theta_loc = theta_loc[0:-1]
-#theta_lab = []
-#for i in range(24):
-#    theta_lab.append(str(i) + ':00')
-#
-## Figure out how to determine a new day in the datetime object for making the polar plots
-## Then use the .seconds feature per 86400 seconds to determine a full day (or use datetime)
-#plt.polar(theta,r)
-#plt.thetagrids(theta_loc,labels = theta_lab)
-#plt.show()
+pplot.polar_flow_plot_per_month(stove,2019,2,t_datetime,gph,'test_pol.png')
+pplot.polar_flow_plot_average_per_month(stove,2019,2,t_datetime,gph,30,'test_pol_ave.png')
