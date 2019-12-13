f12ece562bc8e5fb4a97b2ea951110a603fb442c
tcmorgan2 <tcmorgan2@alaska.edu>
2019-12-13 10:56:38 -0900

    matched tex file names

--- python/puma/bash.py
+++ python/puma/bash.py
@@ -6,17 +6,17 @@ By Douglas Keller
 
 from datetime import datetime
 
-def bash_monthly_reports(year_month):
+def bash_monthly_reports(dateRange):
     """
     Function that writes a bash script to create a unique monthly report zipfile
     
     Arguments:
-        year_month -- year and month in a tuple (year,month)
+        dateRange -- is a pandas date range that the report covers
     """
     
     now = datetime.now()
-    file = 'monthly_report_'+str(year_month[1])+'_'+str(year_month[0])+'_'
-    total_file = 'puma_'+str(year_month[1])+'_'+str(year_month[0])+'_monthly_reports_'+str(now.day)+'_'+str(now.month)+'_'+str(now.year)
+    file = 'monthly_report_'+str(dateRange[0].strftime("%Y_%b"))+'_'+str(dateRange[-1].strftime("%Y_%b"))+'_'
+    total_file = 'puma_'+str(dateRange[0].year)+'_'+str(dateRange[0].month)+'_monthly_reports_'+str(now.day)+'_'+str(now.month)+'_'+str(now.year)
     
     text = r'''
 #Script to make the monthly reports from the python made tex files
